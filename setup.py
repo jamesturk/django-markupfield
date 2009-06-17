f74f509d7eedb89eca5cb62d6f7b3095376df313
James Turk <james.p.turk@gmail.com>
2009-06-17 16:27:00 -0400

    make installable

---
+++ setup.py
@@ -0,0 +1,26 @@
+from distutils.core import setup
+
+long_description = open('README').read()
+
+setup(
+    name='django-markupfield',
+    version="0.1.0",
+    package_dir={'markupfield': 'markupfield'},
+    packages=['markupfield', 'markupfield.tests'],
+    description='Custom Django field for easy use of markup in text fields',
+    author='James Turk',
+    author_email='james.p.turk@gmail.com',
+    license='BSD License',
+    url='http://github.com/jamesturk/django-markupfield/',
+    long_description=long_description,
+    platforms=["any"],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Environment :: Web Environment',
+    ],
+)
