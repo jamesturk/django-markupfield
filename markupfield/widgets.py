816f444bf08151bc7cf3ac93e59b9466a36954fd
James Turk <james.p.turk@gmail.com>
2009-07-19 19:25:24 -0700

    fixed with rendering forms on postgres

--- markupfield/widgets.py
+++ markupfield/widgets.py
@@ -3,7 +3,7 @@ from django.contrib.admin.widgets import AdminTextareaWidget
 
 class MarkupTextarea(forms.widgets.Textarea):
     def render(self, name, value, attrs=None):
-        if value is not None:
+        if value is not None and not isinstance(value, unicode):
             value = value.raw
         return super(MarkupTextarea, self).render(name, value, attrs)
 
