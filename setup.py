ff11e84f50562abc56fafacf1d6b901dac1cb696
James Turk <james.p.turk@gmail.com>
2009-08-05 13:59:05 -0400

    bumped version to 0.3.0-dev

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ long_description = open('README.rst').read()
 
 setup(
     name='django-markupfield',
-    version="0.2.0",
+    version="0.3.0-dev",
     package_dir={'markupfield': 'markupfield'},
     packages=['markupfield', 'markupfield.tests'],
     description='Custom Django field for easy use of markup in text fields',
