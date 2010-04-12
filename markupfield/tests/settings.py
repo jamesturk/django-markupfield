6fdbe51f390a8fd05116745d3bbd2f749067dec3
James Turk <james.p.turk@gmail.com>
2010-04-12 12:06:48 -0400

    added Michael Fladischer's test patch

--- markupfield/tests/settings.py
+++ markupfield/tests/settings.py
@@ -9,10 +9,10 @@ def render_rest(markup):
     parts = publish_parts(source=markup, writer_name="html4css1")
     return parts["fragment"]
 
-MARKUP_FIELD_TYPES = {
-    'markdown': markdown.markdown,
-    'ReST': render_rest,
-}
+MARKUP_FIELD_TYPES = [
+    ('markdown', markdown.markdown),
+    ('ReST', render_rest),
+]
 
 INSTALLED_APPS = (
     'markupfield.tests',
