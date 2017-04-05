3c7d7b6e489859de02676d9f6da755ac2f79c551
James Turk <james.p.turk@gmail.com>
2017-04-04 21:06:39 -0400

    1.4.2

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ long_description = open('README.rst').read()
 
 setup(
     name='django-markupfield',
-    version="1.4.1",
+    version="1.4.2",
     package_dir={'markupfield': 'markupfield'},
     packages=['markupfield', 'markupfield.tests'],
     package_data={'markupfield': ['locale/*/*/*']},
