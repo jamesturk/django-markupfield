f473211ab824e5419c4c00bd0ea4040ad6d25c8d
Michal Čihař <michal@cihar.com>
2018-08-13 14:40:30 +0200

    Add renderer parameter to widget render
    This is required as of Django 2.1.

--- markupfield/widgets.py
+++ markupfield/widgets.py
@@ -4,10 +4,10 @@ from django.contrib.admin.widgets import AdminTextareaWidget
 
 class MarkupTextarea(forms.widgets.Textarea):
 
-    def render(self, name, value, attrs=None):
+    def render(self, name, value, attrs=None, renderer=None):
         if hasattr(value, 'raw'):
             value = value.raw
-        return super(MarkupTextarea, self).render(name, value, attrs)
+        return super(MarkupTextarea, self).render(name, value, attrs, renderer=renderer)
 
 
 class AdminMarkupTextareaWidget(MarkupTextarea, AdminTextareaWidget):
